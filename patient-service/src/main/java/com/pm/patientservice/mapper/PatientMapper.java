6f872ecb569bd5995c7943bd1cf5da3813714577
Harsh Vardhan Singh <harshvv839@gmail.com>
2025-05-27 17:21:03 +0530

    added endpoints in patient controller

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,9 +1,15 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
+import jakarta.servlet.http.PushBuilder;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
+
+    //static method that converts patient object TO patient response DTO
     public static PatientResponseDTO toDTO(Patient patient)
     {
         PatientResponseDTO patientDTO=new PatientResponseDTO();
@@ -13,5 +19,16 @@ public class PatientMapper {
         patientDTO.setEmail(patient.getEmail());
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
         return patientDTO;
+    }
+
+    public static Patient toModel (PatientRequestDTO patientRequestDTO)
+    {
+        Patient patient=new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
